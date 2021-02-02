6ec1e0f6d431dc496547e21adb8e004d06f977fe
up1 <somkiat.p@gmail.com>
2021-02-02 08:53:05 +0700

    Add coverage report

--- backend/sonarqube.js
+++ backend/sonarqube.js
@@ -11,8 +11,8 @@ sonarqubeScanner(
       "sonar.inclusions": "**",
       "sonar.test.inclusions":
         "src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx",
-      //   "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
-      //   "sonar.testExecutionReportPaths": "coverage/test-reporter.xml",
+      "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
+      "sonar.testExecutionReportPaths": "coverage/test-reporter.xml",
     },
   },
   () => {}
