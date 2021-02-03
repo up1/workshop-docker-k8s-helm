d2f19c603568870b4c1ad414af7b6367b3d5237a
pirawatCHAI <pirawat.c@thairath.co.th>
2021-02-03 14:12:22 +0700

    add sona

--- backend/sonarqube.js
+++ backend/sonarqube.js
@@ -4,16 +4,17 @@ sonarqubeScanner(
     serverUrl: "http://128.199.226.142:9000",
     token: "d85ad41b5e8209c6bd23f8dd78563dc9b1257041",
     options: {
-      "sonar.projectName": "Backend",
-      "sonar.projectDescription": "Backend",
+      "sonar.projectName": "Backend_p",
+      "sonar.projectDescription": "Backend_p",
       "sonar.sources": ".",
       "sonar.tests": "src",
+      "sonar.projectKey": "key-test",
       "sonar.inclusions": "**",
       "sonar.test.inclusions":
         "src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx",
-      "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
-      "sonar.testExecutionReportPaths": "coverage/test-reporter.xml",
+      // "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
+      // "sonar.testExecutionReportPaths": "coverage/test-reporter.xml",
     },
   },
-  () => {}
+  () => { }
 );
